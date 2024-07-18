c1d7149708ca870908497e6884774518ecb7de1d
amaana05 <164677417+amaana05@users.noreply.github.com>
2024-07-18 22:36:46 +0530

    dfsdf

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,9 +1,9 @@
 
 const allowedOrigins = [
-    '*'
-    // 'http://localhost:5173',
-    // 'http://localhost:4200',
-    // 'https://blogging5402.netlify.app'
+    'http://localhost:5173',
+    'http://localhost:4200',
+    'https://blogging5142.netlify.app/',
+    'https://blogging5142.netlify.app'
 
 ];
 
